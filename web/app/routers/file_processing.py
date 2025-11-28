c38c8c92f1b24fd37bb303322e05b87b3f280048
Ali Naseri <alinaseri.ut@gmail.com>
2025-11-28 11:11:17 -0500

    Store files locally

--- web/app/routers/file_processing.py
+++ web/app/routers/file_processing.py
@@ -1,16 +1,46 @@
-from fastapi import APIRouter, UploadFile
+from typing import Optional
+from fastapi import APIRouter, HTTPException, UploadFile, File, status
 from web.app.file import FileUpload
+from pathlib import Path
+from uuid import uuid4
+import logging
+
+logging.basicConfig(level=logging.ERROR, format="%(levelname)s: %(message)s")
+logger = logging.getLogger(__name__)
 
 file_router = APIRouter(prefix="/file")
 
+upload_dir = Path(__file__).parent.parent / "temp" / "uploads"
+upload_dir.mkdir(parents=True, exist_ok=True)
+
 
 @file_router.post("/upload")
-def upload_file(file: UploadFile, description: str):
+async def upload_file(file: UploadFile = File(...), description: Optional[str] = None):
 
     file_upload = FileUpload(
         file_name=file.filename,
-        file_data=file.file.read(),
+        file_data=await file.read(),
         file_size=file.size,
         mime_type=file.content_type,
+        description=description,
     )
-    return {"upload": "successful"}
+
+    # save file
+    try:
+        temp_local_path = str(
+            upload_dir / f"{str(uuid4().hex)}-{file_upload.file_name}"
+        )
+        with open(
+            temp_local_path,
+            "w",
+        ) as f:
+            f.write(file_upload.file_data)
+        logger.info("File uploaded successfuly")
+    except Exception as e:
+        logger.exception("Failed to upload File :(")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to save the file",
+        )
+
+    return {"upload": "successful", "path": temp_local_path}
